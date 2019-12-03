aaa46e87675d88285a78ceee0695086ed967db7d
Jay Seo <sjs5953@hotmail.com>
2019-12-03 22:15:40 +0000

    spinner 2

--- spinner1.js
+++ spinner1.js
@@ -1,4 +1,7 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+
+
 setTimeout(() => {
   process.stdout.write('\r|   ');
 }, 100);
---
+++ spinner2.js
@@ -0,0 +1,8 @@
+let char = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   '];
+let timeOut = 100;
+for(let i = 0; i < 10; i++){
+    setTimeout(() => {
+        process.stdout.write(char[i]);
+    }, timeOut);
+    timeOut += 200;
+}
